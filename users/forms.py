f6eaa07e49fc6cea9255c7f268868587e4817ebb
Prashant Piyush <piyush.5862@gmail.com>
2018-01-27 18:31:48 +0530

    fixed email, agree verification on signup

--- users/forms.py
+++ users/forms.py
@@ -10,10 +10,17 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=50, required=True)
     last_name = forms.CharField(max_length=50, required=False)
     email = forms.EmailField(max_length=255, required=True)
+    agree = forms.CharField(max_length=50, required=True, error_messages={'required': 'You have not agreed to terms and conditions.'})
+
+    # def clean_agree(self):
+    #     data = self.clean_data['agree']
+    #     if data != 'agree':
+    #         raise forms.ValidationError('You have not agreed to terms and conditions.')
+    #     return data
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'agree')
 
 class LoginForm(AuthenticationForm):
     # username = forms.CharField(max_length=150, required=False)
--- users/views.py
+++ users/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, redirect, render_to_response
-from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate as auth_authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth import views as auth_views
 
 from .forms import SignUpForm, LoginForm
+from .backends import CustomAuthenticationBackend as user_auth
 
 def signup(request):
     auth_logout(request)
+    # if 'su_agree' not in request.POST:
+    #     form = SignUpForm()
+    #     form.add_error('accept terms and condi')
+    #     return render(request, 'signup.html', {'form': form})
     if request.method == 'POST':
         # request.POST = request.POST.copy()
         # request.POST.__setitem__('username', request.POST['email'])
-        print(list(request.POST.items()))
         signup_request = {
             # 'csrfmiddlewaretoken': request.POST['csrfmiddlewaretoken'],
             'username': request.POST['su_email'],
@@ -21,12 +25,14 @@ def signup(request):
             'password1': request.POST['su_password'],
             'password2': request.POST['su_cpassword']
         }
+        if 'su_agree' in request.POST:
+            signup_request['agree'] = request.POST['su_agree'] 
         form = SignUpForm(signup_request)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = user_auth().authenticate(username=username, password=raw_password)
             if user is not None:
                 auth_login(user=user, request=request)
                 return redirect('/')
@@ -46,7 +52,7 @@ def login(request):
         if form.is_valid() or True:
             email_or_username = login_request['username']
             raw_password = login_request['password']
-            user = authenticate(username=email_or_username, password=raw_password)
+            user = user_auth().authenticate(username=email_or_username, password=raw_password)
             if user is not None:
                 auth_login(user=user, request=request)
                 return redirect('/')
