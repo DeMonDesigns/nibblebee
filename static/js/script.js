2e94acaf6797b693b5a24d886f7f6b2c133c49c3
DeMonDesigns <demongraphicdesigns@gmail.com>
2018-01-30 22:48:22 +0530

    Added Toggle Profile

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     $('.toggle_icon').click(function () {
         $('#toggle_menu').toggleClass('toggle_open');
     });
+    $('.prof_img').click(function () {
+        $('#toggle_profile').toggleClass('toggle_open');
+    });
     $('.search_icon').click(function () {
         $('.toggle_search').toggleClass('fullwidth');
         $('.search_input').toggleClass('fullwidth');
